6a3b6a0e3e2baefa9c1026627d9156b66575ff0a
Oni Khalid Ayòbámi <73073198+onikhalid@users.noreply.github.com>
2025-04-17 11:35:10 +0100

    updates to hustle card

--- src/app/(audience-view)/audience/page.tsx
+++ src/app/(audience-view)/audience/page.tsx
@@ -1,46 +1,31 @@
 import HustleCard from '@/app/shared/HustleCard'
-import { PATTERN_PURPLE_BLACK, PATTERN_PURPLE_GREY, PATTERN_PURPLE_WHITE } from '@/app/shared/HustleCard.PatternTypes'
+import { PATTERN_GREEN_BLACK, PATTERN_ORANGE_BLACK, PATTERN_PURPLE_BLACK, PATTERN_PURPLE_GREY, PATTERN_PURPLE_WHITE } from '@/app/shared/HustleCard.PatternTypes'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
 
-            {/* <HustleCard
-                title='Hairdressing'
-                number={"25"}
-                amount={"$250"}
-                pattern="PATTERN_PURPLE_BLACK"
-
-            />
             <HustleCard
-                title='Hairdressing'
-                number={"25"}
-                amount={"$6000"}
-                pattern="PATTERN_PURPLE_WHITE"
-
-            /> */}
-            
-            <HustleCard
-                title='Hairdressing'
+                id="gaming-card"
+                title="Gaming"
                 number={"25"}
                 amount={"$250"}
                 pattern={PATTERN_PURPLE_WHITE}
-
             />
             <HustleCard
-                title='Hairdressing'
+                id="furniture-card"
+                title="Furniture"
                 number={"25"}
-                amount={"$250"}
+                amount={"$450"}
                 pattern={PATTERN_PURPLE_GREY}
-
             />
             <HustleCard
-                title='Hairdressing'
+                id="welder-card"
+                title="Welder"
                 number={"25"}
-                amount={"$250"}
-                pattern={PATTERN_PURPLE_BLACK}
-
+                amount={"$750"}
+                pattern={PATTERN_ORANGE_BLACK}
             />
         </div>
     )
--- src/app/shared/HustleCard.tsx
+++ src/app/shared/HustleCard.tsx
@@ -4,15 +4,14 @@ import { cn } from "@/utils/classNames";
 
 
 type HustleCardPatternProps = {
-    className?: string
-    title?: string
-    number?: string | number
-    amount?: string | number
-    pattern: string
-    svgProps?: React.SVGProps<SVGSVGElement>
+    className?: string;
+    title?: string;
+    number?: string | number;
+    amount?: string | number;
+    pattern: string;
+    svgProps?: React.SVGProps<SVGSVGElement>;
+    id?: string; // Added unique ID prop
 }
-
-
 
 const HustleCard = ({
     title = "Card",
@@ -21,61 +20,66 @@ const HustleCard = ({
     className,
     pattern,
     svgProps,
+    id = `hustlecard-${Math.random().toString(36).substring(2, 9)}`, // Generate a unique ID if not provided
     ...props
-}: HustleCardPatternProps) => (
-    <article className={cn("flex flex-col border-[3px] border-[#7E3CE0] rounded-3xl max-w-[250px] aspect-video", className)}>
-        <header
-            className="text-3xl text-white font-bold p-2 text-center "
-        >
-            {title}
-        </header>
-        <div className="mt-auto relative">
-
-            <div className="absolute top-0 size-full left-0 right-0 bottom-0 flex flex-col items-center justify-center">
-                <h5
-                    style={{ WebkitTextStroke: "7px black !important;" }}
-                    className="text-8xl text-white font-black "
-                >
-                    {number}
-                </h5>
-                <p
-                    className="text-4xl text-white font-black "
-                    style={{ WebkitTextStroke: "2px black !important;" }}
-                >
-                    {amount}
-                </p>
-            </div>
-
-            <svg
-                viewBox="0 0 121 77"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                {...svgProps}
+}: HustleCardPatternProps) => {
+    const patternId = `pattern-${id}`;
+    const imageId = `image-${id}`;
+    
+    return (
+        <article className={cn("flex flex-col border-[3px] border-[#7E3CE0] rounded-3xl max-w-[250px] aspect-video", className)}>
+            <header
+                className="text-3xl text-white font-bold p-2 text-center"
             >
-                <rect width={121} height={77} rx={7.3522} fill="url(#pattern0_402_1074)" />
-                <defs>
-                    <pattern
-                        id="pattern0_402_1074"
-                        patternContentUnits="objectBoundingBox"
-                        width={1}
-                        height={1}
+                {title}
+            </header>
+            <div className="mt-auto relative">
+                <div className="absolute top-0 size-full left-0 right-0 bottom-0 flex flex-col items-center justify-center">
+                    <h5
+                        style={{ WebkitTextStroke: "7px black" }}
+                        className="text-8xl text-white font-black"
                     >
-                        <use
-                            xlinkHref="#image0_402_1074"
-                            transform="matrix(0.00162338 0 0 0.00255102 0 -0.563776)"
+                        {number}
+                    </h5>
+                    <p
+                        className="text-4xl text-white font-black"
+                        style={{ WebkitTextStroke: "2px black" }}
+                    >
+                        {amount}
+                    </p>
+                </div>
+                <svg
+                    viewBox="0 0 121 77"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    {...svgProps}
+                >
+                    <rect width={121} height={77} rx={7.3522} fill={`url(#${patternId})`} />
+                    <defs>
+                        <pattern
+                            id={patternId}
+                            patternContentUnits="objectBoundingBox"
+                            width={1}
+                            height={1}
+                        >
+                            <use
+                                xlinkHref={`#${imageId}`}
+                                transform="matrix(0.00162338 0 0 0.00255102 0 -0.563776)"
+                            />
+                        </pattern>
+                        <image
+                            id={imageId}
+                            width={616}
+                            height={834}
+                            preserveAspectRatio="none"
+                            xlinkHref={pattern}
                         />
-                    </pattern>
-                    <image
-                        id="image0_402_1074"
-                        width={616}
-                        height={834}
-                        preserveAspectRatio="none"
-                        xlinkHref={pattern}
-                    />
-                </defs>
-            </svg>
-        </div>
-    </article>
-);
+                    </defs>
+                </svg>
+            </div>
+        </article>
+    );
+};
+
 export default HustleCard;
